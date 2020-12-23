7a569981460b6b04f1f84df5221d84a6344a8d4e
itamar <itamarmaouda101@gmail.com>
2020-12-23 06:06:17 -0800

    .

--- src/.rootkit.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/itamar/6/linux_kernel_rootkit/src/rootkit.mod := { echo  /home/itamar/6/linux_kernel_rootkit/src/rootkit.o; echo; } > /home/itamar/6/linux_kernel_rootkit/src/rootkit.mod
--- src/rootkit.mod
+++
@@ -1,2 +0,0 @@
-/home/itamar/6/linux_kernel_rootkit/src/rootkit.o
-
